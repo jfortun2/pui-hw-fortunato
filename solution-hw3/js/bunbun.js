8ae7f4e67b355ba49ef2f52ae1182489eb1cd3cd
jfortun2 <112339892+jfortun2@users.noreply.github.com>
2022-09-25 23:21:51 -0400

    Get value

--- solution-hw3/js/bunbun.js
+++ solution-hw3/js/bunbun.js
@@ -1,5 +1,22 @@
-function glazingChange(element){
-    //get value of selected glazing option
-    const priceChange = element.value;
-    //add my code to update the price
+function getGlazeValue(){
+    let selectedGlaze  = document.getElementById("glazingOptions").value;
+    console.log(selectedGlaze);
 }
+
+function updatePrice() {
+    let newPrice = document.getElementById("price").value;
+    console.log(newPrice);
+    let updatePrice = newPrice + selectedGlaze;
+    console.log(updatePrice);
+
+}
+
+function updateText(){
+    let output = document.getElementById("glazingOptions").value;
+    document.querySelector('update').textContent = ouput;
+}
+
+//assign value to drop downs
+//add to starting price
+//multiply by amount chosen
+//update price at bottom on dropdown change
