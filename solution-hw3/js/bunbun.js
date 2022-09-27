72acd864ee97ef188b0da1dcb9e38385e97e2e94
jfortun2 <112339892+jfortun2@users.noreply.github.com>
2022-09-26 21:10:32 -0400

    Testing out some code

--- solution-hw3/js/bunbun.js
+++ solution-hw3/js/bunbun.js
@@ -1,15 +1,57 @@
-function getGlazeValue(){
-    let selectedGlaze  = document.getElementById("glazingOptions").value;
-    console.log(selectedGlaze);
+function glazingChange(element){
+    const priceChange = element.value
 }
 
-function updatePrice() {
-    let newPrice = document.getElementById("price").value;
-    console.log(newPrice);
-    let updatePrice = newPrice + selectedGlaze;
-    console.log(updatePrice);
+let allGlaze = [
+
+    {
+    glaze: 'Keep original',
+    value: '0.00',
+},
+
+{
+    glaze: 'Sugar milk',
+    value: '0.00',
+},
+
+{
+    glaze: 'Vanilla milk',
+    value: '0.50',
+},
+
+{
+    glaze: 'Double chocolate',
+    value: '1.50',
+}
+];
+
+
+function displayValue(glaze){
+    let glazeValue = document.querySelector('#price');
+    glazeValue.innerText = glaze.value;
+}
+
+function onSelectValueChange() {
+    console.log("you selected" + this.value);
+
+    let glazeIndex = parseInt(this.value);
+
+    let glazeToDisplay = allGlaze[glazeIndex];
+
+    displayValue(glazeToDisplay);
 
 }
+
+let selectElement = document.querySelector ('#glaze-select');
+selectElement.addEventListener('change', onSelectValueChange);
+
+
+
+
+
+
+
+
 
 
 
